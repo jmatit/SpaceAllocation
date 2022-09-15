b66ea0eee310f39b387f35bc5c389bd81979cb61
Дмитрий Гаврилей <gavriley94@yandex.ru>
2022-09-15 15:54:00 +0300

    Добавьте файлы проекта.

---
+++ SpaceAllocation.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <new>
+
+int main(int argc, char* argv[])
+{
+	double* pointer;
+	int counter = 1;
+	do
+	{
+		try
+		{
+			pointer = new double[100000];
+		}
+		catch (std::bad_alloc err)
+		{
+			std::cout << "error in space allocation\n";
+			return 1;
+		}
+		std::cout << "space allocation in progress, counter: "<<counter<<'\n';
+		counter++;
+	} while (pointer);
+	std::cout << "\n";
+	return 0;
+}
